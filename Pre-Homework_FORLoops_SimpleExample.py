f1f93c17a934a89f3975a800513c4ea9447656da
44224 <44224@PC004145.coll.lr.local>
2014-10-13 09:33:47 +0100

    First

---
+++ Pre-Homework_FORLoops_SimpleExample.py
@@ -0,0 +1,9 @@
+# William Craddock
+# 13/10/2012
+# Pre-Homework - FOR Loops - Simple Example
+
+# This simple for loop example would write "hello world" 5 times:
+
+for counter in range(5):
+    print("Hello World")
+
